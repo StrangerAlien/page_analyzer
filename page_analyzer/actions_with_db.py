d5786944a77a58dedb31763935f26a69caae4dc3
aleksei <jiunov@gmail.com>
2024-07-10 07:08:19 +0600

    fix chesk#1

--- page_analyzer/actions_with_db.py
+++ page_analyzer/actions_with_db.py
@@ -1,100 +1,105 @@
-import psycopg2
+from psycopg2.pool import SimpleConnectionPool
 from page_analyzer.secrets import DATABASE_URL
 from datetime import datetime
 
-
-def connect_db():
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-    except Exception as error:
-        print('Can`t establish connection to database')
-        raise error
-    return conn
+postgresql_pool = SimpleConnectionPool(1, 20, dsn=DATABASE_URL)
 
 
 def save_url(url):
-    with connect_db() as conn:
-        with conn.cursor() as curs:
-            curs.execute('''
-                INSERT INTO urls (
-                    name,
-                    created_at)
-                VALUES (%s, %s)
-                RETURNING id;''', (url, datetime.now()))
-            url_data = curs.fetchone()
-            conn.commit()
-            return url_data
+    conn = postgresql_pool.getconn()
+    curs = conn.cursor()
+    curs.execute('''
+        INSERT INTO urls (
+            name,
+            created_at)
+        VALUES (%s, %s)
+        RETURNING id;''', (url, datetime.now()))
+    url_data = curs.fetchone()
+    conn.commit()
+    curs.close()
+    postgresql_pool.putconn(conn)
+    return url_data
 
 
 def save_url_check(tags_data):
-    with connect_db() as conn:
-        with conn.cursor() as curs:
-            curs.execute('''
-                INSERT INTO url_checks (
-                    url_id,
-                    status_code,
-                    h1,
-                    title,
-                    description,
-                    created_at )
-                VALUES (%s, %s, %s, %s, %s, %s)''', (
-                tags_data['id'],
-                tags_data['code'],
-                tags_data['h1'],
-                tags_data['title'],
-                tags_data['description'],
-                datetime.now()))
-            conn.commit()
+    conn = postgresql_pool.getconn()
+    curs = conn.cursor()
+    curs.execute('''
+        INSERT INTO url_checks (
+            url_id,
+            status_code,
+            h1,
+            title,
+            description,
+            created_at )
+        VALUES (%s, %s, %s, %s, %s, %s)''', (
+        tags_data['id'],
+        tags_data['code'],
+        tags_data['h1'],
+        tags_data['title'],
+        tags_data['description'],
+        datetime.now()))
+    conn.commit()
+    curs.close()
+    postgresql_pool.putconn(conn)
 
 
 def get_data_by_id(url_id):
-    with connect_db() as conn:
-        with conn.cursor() as curs:
-            curs.execute('''
-                SELECT * FROM urls WHERE id=%s''', (url_id,))
-            existing = curs.fetchone()
-            return existing
+    conn = postgresql_pool.getconn()
+    curs = conn.cursor()
+    curs.execute('''
+        SELECT * FROM urls WHERE id=%s''', (url_id,))
+    existing = curs.fetchone()
+    curs.close()
+    postgresql_pool.putconn(conn)
+    return existing
 
 
 def get_data_by_name(url):
-    with connect_db() as conn:
-        with conn.cursor() as curs:
-            curs.execute('''
-                SELECT * FROM urls WHERE name=%s''', (url,))
-            existing = curs.fetchone()
-            return existing
+    conn = postgresql_pool.getconn()
+    curs = conn.cursor()
+    curs.execute('''
+        SELECT * FROM urls WHERE name=%s''', (url,))
+    existing = curs.fetchone()
+    curs.close()
+    postgresql_pool.putconn(conn)
+    return existing
 
 
 def get_data_all_urls():
-    with connect_db() as conn:
-        with conn.cursor() as curs:
-            curs.execute('''
-                SELECT
-                    urls.id,
-                    urls.name,
-                    url_checks.status_code,
-                    MAX(url_checks.created_at) AS last_check
-                FROM urls
-                LEFT JOIN url_checks ON urls.id = url_checks.url_id
-                GROUP BY urls.id, urls.name, url_checks.status_code
-                ORDER BY urls.id DESC;''', )
-            url_checks = curs.fetchall()
-            return url_checks
+    conn = postgresql_pool.getconn()
+    curs = conn.cursor()
+    curs.execute('''
+        SELECT
+            urls.id,
+            urls.name,
+            url_checks.status_code,
+            MAX(url_checks.created_at) AS last_check
+        FROM urls
+        LEFT JOIN url_checks ON urls.id = url_checks.url_id
+        GROUP BY urls.id, urls.name, url_checks.status_code
+        ORDER BY urls.id DESC;''', )
+    url_checks = curs.fetchall()
+    curs.close()
+    postgresql_pool.putconn(conn)
+    return url_checks
 
 
 def get_url_check(url_id):
-    with connect_db() as conn:
-        with conn.cursor() as curs:
-            curs.execute('''
-                SELECT
-                    id,
-                    status_code,
-                    h1,
-                    title,
-                    description,
-                    created_at
-                FROM url_checks
-                WHERE url_id = %s
-                ORDER BY id DESC''', (url_id,))
-            checks = curs.fetchall()
-            return checks
+    conn = postgresql_pool.getconn()
+    curs = conn.cursor()
+    curs.execute('''
+        SELECT
+            id,
+            status_code,
+            h1,
+            title,
+            description,
+            created_at
+        FROM url_checks
+        WHERE url_id = %s
+        ORDER BY id DESC''', (url_id,))
+    checks = curs.fetchall()
+    curs.close()
+    postgresql_pool.putconn(conn)
+    return checks
